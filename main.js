314d264c31145ac704234fed1912ec4067ebfb26
dbxdemba <67698220+dbxdemba@users.noreply.github.com>
2022-12-21 13:58:49 +0100

    Update main.js

--- main.js
+++ main.js
@@ -3,8 +3,8 @@ const submitbtn = document.querySelector('.submit-button');
 const container1 = document.querySelector('.container-item-1');
 const container2 = document.querySelector('.container-item-2');
 const spanactive = document.querySelector('.btn-selected-text');
+const maincont = document.querySelector('.container')
 const newcont2 = container2.parentNode.removeChild(container2);
-const maincont = document.querySelector('.container');
 submitbtn.addEventListener('click', function(){
     /*condition servant a verifier l'existence d'une classe. si elle existe alors ca ne se declenche pas
     sinon elle se declenche d'ou la presence de '!' pour dire difference*/
@@ -37,9 +37,23 @@ btnitems.forEach(element => {
 function sub(){
     container2.classList.add('container-item-2-active');
     container1.classList.add('container-item-1-active');
-    container1.parentNode.removeChild(container1);
+    const newcont3 = container1.parentNode.removeChild(container1);
     maincont.appendChild(newcont2);
     spanactive.style.color = 'hsl(25, 97%, 53%)';
     spanactive.style.fontSize = '15px'
+
+    let reseto = document.querySelector('.reset-button');
+    function resetf(){
+        newcont2.remove()
+        maincont.appendChild(newcont3);
+        console.log(newcont3)
+        container1.classList.remove('container-item-1-active');
+        btnitems.forEach(element => {
+            element.classList.remove('btn-active');
+        });
+    }
+    reseto.addEventListener('click', resetf)
 };
+
+
 // container1.firstElementChild.classList.toggle('img-container-active');
